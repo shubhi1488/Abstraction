196f7397a6b962e021811e8e762ca8a240d2afc0
HP <shubhi1488@gmail.com>
2021-12-20 18:12:36 +0530

    Initial commit

---
+++ src/Bike.java
@@ -0,0 +1,16 @@
+//Abstarction
+abstract class Bike {
+    abstract void run();//method contains only body it doesnot contain any implementation 
+}
+class Honda extends Bike{
+
+    @Override
+    void run() {
+        System.out.println("Bike is running");
+    }
+
+    public static void main(String[] args) {
+        Bike obj=new Honda();
+        obj.run();
+    }
+}
